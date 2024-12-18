3e8b5828dd9689e2c2991cf371c7ae871bdee9b9
WajidAhmed-25 <wajidsaleem693@gmail.com>
2024-12-19 01:51:41 +0500

    add images on slider

--- src/Components/HomePage/SwipperSliderMain/index.js
+++ src/Components/HomePage/SwipperSliderMain/index.js
@@ -1,47 +1,138 @@
-import React,{ useRef, useState } from 'react'
+
+// old code
+
+// import React,{ useRef, useState } from 'react'
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+// import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+// import './swiper.css'
+
+// const index = () => {
+//   return (
+//     <div className=''>
+// <>
+//       <Swiper
+//         spaceBetween={30}
+//         centeredSlides={true}
+//         autoplay={{
+//           delay: 2500,
+//           disableOnInteraction: false,
+//         }}
+//         pagination={{
+//           clickable: true,
+//         }}
+//         navigation={true}
+//         modules={[Autoplay, Pagination, Navigation]}
+//         className="mySwiper"
+//       >
+//         <SwiperSlide>Slide</SwiperSlide>
+//         <SwiperSlide>Slide 2</SwiperSlide>
+//         <SwiperSlide>Slide 3</SwiperSlide>
+//         <SwiperSlide>Slide 4</SwiperSlide>
+//         <SwiperSlide>Slide 5</SwiperSlide>
+//         <SwiperSlide>Slide 6</SwiperSlide>
+//         <SwiperSlide>Slide 7</SwiperSlide>
+//         <SwiperSlide>Slide 8</SwiperSlide>
+//         <SwiperSlide>Slide 9</SwiperSlide>
+//       </Swiper>
+//     </>
+
+//     </div>
+//   )
+// }
+
+// export default index;
+
+
+// old code
+
+
+// ____________________________________________________________________________________________________________________________________
+
+
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import './swiper.css'
+import './swiper.css';
 
 
-const index = () => {
+const Index = () => {
+
+  
+const imageUrls = [
+  "https://angeethipk.com//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1662114496-Header-02.jpg%3Fver%3D10&w=3840&q=90",
+  "https://angeethipk.com//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1662114503-Header-03.jpg%3Fver%3D10&w=3840&q=90",
+
+  "https://www.delizia.pk//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1673694005-1.png%3Fver%3D10&w=3840&q=90",
+  "https://www.delizia.pk//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1673694012-2-(1).png%3Fver%3D10&w=3840&q=90",
+
+  "https://foodsinn.co//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1728387899-Foods-Inn-Banner-(1)Artboard-1-copy-2.jpg%3Fver%3D10&w=3840&q=90",
+  "https://foodsinn.co//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1728387909-Foods-Inn-Banner-(1)Artboard-1.jpg%3Fver%3D10&w=3840&q=90",
+  "https://hot-nspicy.com//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1693313374-2.jpg%3Fver%3D10&w=3840&q=90",
+  "https://hot-nspicy.com//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1693313378-3.jpg%3Fver%3D10&w=3840&q=90",  
+  "https://www.kaybees.com.pk//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1729153473-Header-1.jpg%3Fver%3D10&w=3840&q=90",
+  "https://www.kaybees.com.pk//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1729150539-Header-2.jpg%3Fver%3D10&w=3840&q=90",
+  "https://www.kaybees.com.pk//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1729153477-Header-3.jpg%3Fver%3D10&w=3840&q=90",
+ 
+  "https://www.pizzapoint.com.pk//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1728632547-Main-Banner-2.jpg%3Fver%3D10&w=3840&q=90",
+  "https://www.pizzapoint.com.pk//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1731072975-Pizza-Point.jpg%3Fver%3D10&w=3840&q=90",
+  "https://jhr.tooso.pk//_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fimages%2F1727443445-20-BANNER.jpg%3Fver%3D10&w=3840&q=90"
+];
+
+const [randomizedImages, setRandomizedImages] = useState([]);
+
+useEffect(() => {
+  // Fisher-Yates shuffle algorithm
+  const shuffleArray = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
+  setRandomizedImages(shuffleArray(imageUrls));
+}, []);
+
   return (
-    <div className=''>
-<>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+    <div className="w-full">
+      <>
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {randomizedImages.map((imageUrl, index) => (
+        <SwiperSlide key={index}>
+          <img 
+            src={imageUrl} 
+            alt={`Slide ${index + 1}`}
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
     </>
-      
-    </div>
+  </div>
   )
 }
 
-export default index
+export default Index;
 
 
 
