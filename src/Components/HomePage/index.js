4078464372a977a20cf6dd6f0262b8929440989f
WajidAhmed-25 <wajidsaleem693@gmail.com>
2024-12-21 01:53:32 +0500

    framer-motion

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,40 +1,23 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
 
-
-// Navbar , Footer , Dropdown
-
-import Navbar from '../Navbar/index'
-import Dropdown from './Dropdowns/index'
-import Footer from '../Footer/index'
-
-// Components Import //
-
-
-import Slider from './SwipperSliderMain/index'
-import BrandSlider from './BrandsSlider/index'
-import DealsSlider from './DealsSlider'
-import ProductCards from './ProductCards/index'
-import CardsShufftler from './CardsShufftler/index'
-import WeeklyTopVendors from './MappingCards'
-import DeliveryBanner from './BannerDiv'
-import OrganicFoodGrid from './MovingCards'
-import ProductShowcase from './FourProductsDiv'
-import FeaturesSection from './Banners'
-
-
-// ----- Scroll Top ----//
-
-import ScrollToTop from '../ScrollArrow'
-
-
-
-
-// ---------- AOS Animation ---------------//
-
+// Existing imports
+import Navbar from '../Navbar/index';
+import Dropdown from './Dropdowns/index';
+import Footer from '../Footer/index';
+import Slider from './SwipperSliderMain/index';
+import BrandSlider from './BrandsSlider/index';
+import DealsSlider from './DealsSlider';
+import ProductCards from './ProductCards/index';
+import CardsShufftler from './CardsShufftler/index';
+import WeeklyTopVendors from './MappingCards';
+import DeliveryBanner from './BannerDiv';
+import OrganicFoodGrid from './MovingCards';
+import ProductShowcase from './FourProductsDiv';
+import FeaturesSection from './Banners';
+import ScrollToTop from '../ScrollArrow';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
-
 
 const Index = () => {
   useEffect(() => {
@@ -50,43 +33,159 @@ const Index = () => {
     });
   }, []);
 
+  const staggerContainer = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+        delayChildren: 0.1
+      }
+    }
+  };
+
+  const staggerItem = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 10
+      }
+    }
+  };
 
   return (
     <div className='bg-white'>
-
-{/* <Navbar/>  
-<Dropdown/>    
+      <Navbar />
       
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <Dropdown />
+        </motion.div>
+      </motion.div>
 
-<Slider/> */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <Slider />
+        </motion.div>
+      </motion.div>
 
-  <Navbar />
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <BrandSlider />
+        </motion.div>
+      </motion.div>
 
-<div data-aos="zoom-in">
-  <Dropdown />
-</div>
-<div data-aos="fade-up">
-  <Slider />
-</div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <DealsSlider />
+        </motion.div>
+      </motion.div>
 
-<BrandSlider/>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <ProductCards />
+        </motion.div>
+      </motion.div>
 
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <CardsShufftler />
+        </motion.div>
+      </motion.div>
 
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <WeeklyTopVendors />
+        </motion.div>
+      </motion.div>
 
-<DealsSlider/>
-<ProductCards/>
-<CardsShufftler/>
-<WeeklyTopVendors/>
-<DeliveryBanner/>
-<OrganicFoodGrid/>
-<ProductShowcase/>
-<FeaturesSection/>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <DeliveryBanner />
+        </motion.div>
+      </motion.div>
 
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <OrganicFoodGrid />
+        </motion.div>
+      </motion.div>
 
-<ScrollToTop/>
-<Footer/>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <ProductShowcase />
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer}
+      >
+        <motion.div variants={staggerItem}>
+          <FeaturesSection />
+        </motion.div>
+      </motion.div>
+
+      <ScrollToTop />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
